671c4848319a436ee18614536765cbc7a0f75bd2
J-AGGERHOLM <jajo0004@stud.kea.dk>
2025-11-11 10:57:48 +0100

    text shows up

--- js/sendMessage.js
+++ js/sendMessage.js
@@ -1,26 +1,33 @@
 const form = document.querySelector('#messageForm');
 const URL = "http://localhost:8080/";
-const ingredients = document.querySelector("#ingredients");
+const ingredients = document.getElementById("ingredients");
+const recipeOutput = document.querySelector("#RecipeOutput");
 
-async function getRecipe() {
+async function getRecipe(event) {
+    event.preventDefault();
 
-    const raw = ingredients.value;
-    const ingredients = raw
-        .split(",");
+      const ingredient = ingredients.value.split(",");
+
 
 try {
 
     const response = await fetch(URL + "chat", {
         method: 'POST',
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(ingredients)
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(ingredient)
     })
     if (!response.ok) {
         throw new Error(response.statusText);
     }
 
     const data = await response.json();
+    recipeOutput.textContent = data.Choices[0].message.content;
+    console.log(data)
 
+
+}catch(err) {
+console.log(err);}
+}
 
 
 form.addEventListener("submit", getRecipe);
