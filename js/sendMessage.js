24d47553094caa2b6a08aed1681b98b9a87ca71d
J-AGGERHOLM <jajo0004@stud.kea.dk>
2025-11-11 10:28:51 +0100

    wip

--- js/sendMessage.js
+++ js/sendMessage.js
@@ -1,2 +1,26 @@
 const form = document.querySelector('#messageForm');
-const URL
+const URL = "http://localhost:8080/";
+const ingredients = document.querySelector("#ingredients");
+
+async function getRecipe() {
+
+    const raw = ingredients.value;
+    const ingredients = raw
+        .split(",");
+
+try {
+
+    const response = await fetch(URL + "chat", {
+        method: 'POST',
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(ingredients)
+    })
+    if (!response.ok) {
+        throw new Error(response.statusText);
+    }
+
+    const data = await response.json();
+
+
+
+form.addEventListener("submit", getRecipe);
